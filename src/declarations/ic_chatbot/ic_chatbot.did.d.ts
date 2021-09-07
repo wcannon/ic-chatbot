e81c878ccdbb6c59d7b19398d2cedd8f17ac2a2d
Dylan Miller <dylancm4@users.noreply.github.com>
2021-09-07 12:51:52 -0700

    Delete src/declarations directory

--- src/declarations/ic_chatbot/ic_chatbot.did.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-export type JsonText = string;
-export type SessionId = string;
-export type UserInput = string;
-export interface _SERVICE {
-  'get_next_block' : (arg_0: SessionId, arg_1: UserInput) => Promise<JsonText>,
-  'init_session' : () => Promise<JsonText>,
-}
